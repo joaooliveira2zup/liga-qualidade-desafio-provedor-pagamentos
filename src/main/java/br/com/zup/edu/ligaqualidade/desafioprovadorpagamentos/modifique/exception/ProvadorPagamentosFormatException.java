e439277a13ed4c300ea636c339dfe14cfa7128ab
joaooliveira2zup <73232024+joaooliveira2zup@users.noreply.github.com>
2021-03-06 14:10:14 -0300

    Create ProvadorPagamentosFormatException.java

---
+++ src/main/java/br/com/zup/edu/ligaqualidade/desafioprovadorpagamentos/modifique/exception/ProvadorPagamentosFormatException.java
@@ -0,0 +1,12 @@
+package br.com.zup.edu.ligaqualidade.desafioprovadorpagamentos.modifique.exception;
+
+public class ProvadorPagamentosFormatException extends ProvadorPagamentosException {
+
+	public ProvadorPagamentosFormatException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ProvadorPagamentosFormatException(String message) {
+		super(message);
+	}
+}
