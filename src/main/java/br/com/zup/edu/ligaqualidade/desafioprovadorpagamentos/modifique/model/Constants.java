3eb2e2c761507612df84f675ed2aebd343d4d417
joaooliveira2zup <73232024+joaooliveira2zup@users.noreply.github.com>
2021-03-06 14:11:40 -0300

    Create Constants.java

---
+++ src/main/java/br/com/zup/edu/ligaqualidade/desafioprovadorpagamentos/modifique/model/Constants.java
@@ -0,0 +1,7 @@
+package br.com.zup.edu.ligaqualidade.desafioprovadorpagamentos.modifique.model;
+
+public class Constants {
+	private Constants() {}
+
+	public static final String DATE_FORMAT = "dd/MM/yyyy";
+}
